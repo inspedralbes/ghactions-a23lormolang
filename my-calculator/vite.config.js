8cf6911d3ea7bfb2c6ae5c8a57bfb31fe4cfdfaf
Lorenzo <a23lormolang@inspedralbes.cat>
2025-02-28 14:46:30 +0100

    Fin

--- my-calculator/vite.config.js
+++ my-calculator/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/myapp/',
+  base: '/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
