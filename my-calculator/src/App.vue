f0c80713823107a1b278beed02a1784ec2415f8c
Lorenzo <a23lormolang@inspedralbes.cat>
2025-02-28 13:07:23 +0100

    Cambios para utilizar actions con Vue

---
+++ my-calculator/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="calculator">
+    <div v-for="num in numbers" :key="num" class="button" @click="showNumber(num)">
+      {{ num }}
+    </div>
+    <p v-if="selectedNumber">Has clicat: {{ selectedNumber }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const selectedNumber = ref(null);
+
+const showNumber = (num) => {
+  selectedNumber.value = num;
+};
+</script>
+
+<style>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  width: 200px;
+  margin: auto;
+}
+
+.button {
+  padding: 20px;
+  background: lightgray;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 5px;
+}
+</style>
